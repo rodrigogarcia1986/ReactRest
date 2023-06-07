0d120483feb324ad6bfc2a363f5e7606cb94c29a
rodrigogarcia1986 <rodrigogarciacapota@gmail.com>
2023-06-07 17:43:00 -0300

    Enhanced

--- index.js
+++ index.js
@@ -102,5 +102,5 @@ app.post('/api/persons/', (req, res) => {
     }
 })
 
-const PORT = 3001;
-app.listen(3001, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
