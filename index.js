541a93cec0a147e8a46779cdbeb431860d14314c
rodrigogarcia1986 <rodrigogarciacapota@gmail.com>
2023-06-12 16:51:09 -0300

    Exercises untill 3.22 done

--- index.js
+++ index.js
@@ -1,106 +1,168 @@
 const express = require('express');
+const person = require('./models/person');
 var morgan = require('morgan');
+const cors = require('cors')
 
 morgan.token('data', function (req) { return JSON.stringify(req.body) })
 
-
 const app = express();
+
+app.use(cors())
+
+app.use(express.static('build'))
 
 app.use(express.json());
 
 app.use(morgan(':method :url :status :response-time :data'))
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+const errorHandler = (error, req, res, next) => {
+    console.log("Chegou no error handler")
 
-const fixedId = persons.length;
-let newId = fixedId + 1;
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        console.log("entrou neste if")
+        return res.status(400).send(error.message)
+    }
+
+    //next(error)
+    //next()
+}
+
+
+// let persons = [
+//     {
+//         "id": 1,
+//         "name": "Arto Hellas",
+//         "number": "040-123456"
+//     },
+//     {
+//         "id": 2,
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523"
+//     },
+//     {
+//         "id": 3,
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345"
+//     },
+//     {
+//         "id": 4,
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122"
+//     }
+// ]
+
+// const fixedId = persons.length;
+// let newId = fixedId + 1;
 
 app.get('/api/persons', (req, res) => {
 
-    const total = persons.length;
-    req.timestamp = new Date();
+    person.find({}).then(person => res.json(person))
 
-    const result = `<p>Phonebook has info for ${total} people</p> <br/><p>${req.timestamp}<p>`;
-
-    res.json(result);
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
 
     const { id } = req.params;
 
-    const result = persons.find(person => person.id === Number(id));
+    // const result = persons.find(person => person.id === Number(id));
 
-    if (isNaN(Number(id))) {
-        return res.status(400).json({ error: "Id must be a valid number!" });
-    } else if (result === undefined) {
-        return res.status(404).json({ error: "Name must be unique!" });
-    } else {
-        return res.json(result);
-    }
+    // if (isNaN(Number(id))) {
+    //     return res.status(400).json({ error: "Id must be a valid number!" });
+    // } else if (result === undefined) {
+    //     return res.status(404).json({ error: "Name must be unique!" });
+    // } else {
+    //     return res.json(result);
+    // }
+
+    person.findById(id).then(response => {
+        if (response) {
+            res.json(response)
+        } else {
+            res.status(404).end()
+        }
+    })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res) => {
 
     const { id } = req.params;
 
-    const existingContact = persons.find(person => person.id === Number(id));
+    // const existingContact = persons.find(person => person.id === Number(id));
 
-    if (isNaN(Number(id))) {
-        return res.status(400).json({ error: "Id must be a valid number!" });
-    } else if (existingContact === undefined) {
-        return res.status(404).json({ error: "There is no contact with the informed id" });
-    } else {
-        persons = persons.filter(person => person.id !== Number(id))
-        return res.status(204).send();
-    }
+    // if (isNaN(Number(id))) {
+    //     return res.status(400).json({ error: "Id must be a valid number!" });
+    // } else if (existingContact === undefined) {
+    //     return res.status(404).json({ error: "There is no contact with the informed id" });
+    // } else {
+    //     persons = persons.filter(person => person.id !== Number(id))
+    //     return res.status(204).send();
+    // }
+
+    person.findByIdAndDelete(id).then(response => res.json(response))
+
 })
 
-app.post('/api/persons/', (req, res) => {
+app.post('/api/persons/', (req, res, next) => {
 
     const { name, number } = req.body;
+    console.log("name and number", name, number)
 
-    const existingContact = persons.find(person => person.name === name)
-
+    // const existingContact = person.find(person => person.name === name)
 
     if (!name || !number) {
-        return res.status(400).json({ error: "Name and number must be informed!" });
-    } else if (existingContact !== undefined) {
-        return res.status(400).json({ error: "The informed name is alread being used!" });
-    } else {
-
-        const newObj = {
-            id: newId,
-            name,
-            number
-        }
-
-        persons.push(newObj);
-
-        return res.status(201).send();
+        return res.status(400).json({ error: "Name and number must be informed!" })
     }
+
+    person.find({}).then(persons => {
+        //console.log('persons: ', persons)
+
+        if (persons.some(person => person.name === name)) {
+            console.log("name must be unique")
+            return response.status(400).json({
+                error: 'name must be unique'
+            })
+
+        } else {
+
+            let newPerson = new person({
+                name: name,
+                number: number
+            })
+
+            newPerson.save().then(result => {
+                console.log("Person to save", result)
+                res.json(result)
+            })
+                .catch(error => {
+                    console.log("Chegou no catch:", error.message)
+                    next(error)
+                    //res.json(error)
+                })
+        }
+    })
 })
 
-const PORT = process.env.PORT || 3001;
+app.put('/api/persons/:id', (req, res, next) => {
+    const { id } = req.params
+    const { number } = req.body
+
+    const newPerson = {
+        number
+    }
+
+    person.findByIdAndUpdate(id, newPerson, { new: true })
+        .then(result => res.json(result))
+        .catch(error => next(error))
+
+})
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ models/person.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        minLength: 3,
+        required: [true, 'User name required']
+    },
+    // number: String,
+    number:
+    {
+        type: String,
+        validate: {
+            validator: function (v) {
+                return /\d{11}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number! Phone number must have 11 digits!`
+        },
+        required: [true, 'User phone number required']
+    }
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)
